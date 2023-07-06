ffae7b081e17b86b31f2c465450fb28ff112971a
john.tg <john.tg177@gmail.com>
2023-07-06 23:38:03 +0530

    react query

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,16 +10,20 @@ const Navbar = async () => {
   return (
     <div className="fixed top-0 inset-x-0 bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-3">
-        <div className="flex items-center">
-          <Image
-            src="/treasure.png"
-            width={45}
-            height={45}
-            className="object-fit cursor-pointer"
-            alt="Picture of the author"
-          />
-          <p className="hidden text-zinc-800 text-lg ml-1 md:block">Plunder</p>
-        </div>
+        <Link href={"/"}>
+          <div className="flex items-center">
+            <Image
+              src="/treasure.png"
+              width={45}
+              height={45}
+              className="object-fit cursor-pointer"
+              alt="Picture of the author"
+            />
+            <p className="cursor-pointer hidden text-zinc-800 text-lg ml-1 md:block">
+              Plunder
+            </p>
+          </div>
+        </Link>
         {session?.user ? (
           <UserAccountNav user={session?.user} />
         ) : (
