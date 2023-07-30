20663381c195c1ec3098e2f4aceecbc4ada529c7
john.tg <john.tg177@gmail.com>
2023-07-30 15:52:37 +0530

    error handling and onsucess page

--- src/app/l/create/page.tsx
+++ src/app/l/create/page.tsx
@@ -1,19 +1,20 @@
 "use client";
 
+import { useCustomToast } from "@/components/hooks/use-custom-toast";
+import { toast } from "@/components/hooks/use-toast";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { CreateTopicPayload } from "@/lib/validators/topic";
 import { useMutation } from "@tanstack/react-query";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { FC, useState } from "react";
 
-interface pageProps {}
-
-const page: FC<pageProps> = ({}) => {
+const page: FC = ({}) => {
   const [input, setInput] = useState<string>("");
   const router = useRouter();
+  const { loginToast } = useCustomToast();
 
   const { mutate: createTopic, isLoading } = useMutation({
     mutationFn: async () => {
@@ -22,6 +23,37 @@ const page: FC<pageProps> = ({}) => {
       };
       const { data } = await axios.post("/api/topic", payload);
       return data as string;
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 409) {
+          return toast({
+            title: "Topic already exists.",
+            description: "Please choose a diffrent topic name.",
+            variant: "destructive",
+          });
+        }
+
+        if (err.response?.status === 422) {
+          return toast({
+            title: "Invalid topic name",
+            description: "Please choose a name between 3 and 21 characters.",
+            variant: "destructive",
+          });
+        }
+
+        if (err.response?.status === 401) {
+          return loginToast();
+        }
+      }
+      toast({
+        title: "There was an error.",
+        description: "Could not create topic.",
+        variant: "destructive",
+      });
+    },
+    onSuccess: (data) => {
+      router.push(`/r/${data}`);
     },
   });
 
